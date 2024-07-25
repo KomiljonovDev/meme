f264904d9fc0ba9789efde240151b49aea547e4e
Komiljonovdev <uzokdeveloper@gmail.com>
2024-07-26 02:34:16 +0500

    update

--- app/Telegram/Bot/BotHandler.php
+++ app/Telegram/Bot/BotHandler.php
@@ -6,7 +6,6 @@ use App\Telegram\Bot\Bot;
 
 class BotHandler extends Bot {
     public function start(int $chatId) {
-        echo "test";
         $this->sendChatAction('typing', $chatId)
             ->sendMessage('Assalomu alaykum');
     }
