22a613057611a374aafae981960c075ae17fc5b0
Komiljonovdev <uzokdeveloper@gmail.com>
2024-07-26 02:27:43 +0500

    update

--- app/Telegram/Bot/Handler.php
+++ app/Telegram/Bot/Handler.php
@@ -2,9 +2,7 @@
 use App\Telegram\Bot\BotHandler;
 
 $bot = new BotHandler(['botToken'=>'7170488644:AAG3pMlvLYfvGoGpoL9P3taX5FEDI37MOm0']);
-$data = $bot->setWebHook('https://komiljonovdev.uz/okdeveloper/tgbots/meme/public/webhook');
 
-var_dump($data->result());
 
 $update = $bot->updates();
 
