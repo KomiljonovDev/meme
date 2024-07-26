a564e39d24035e8b5c7df05c3406d14d9b0dec72
Komiljonovdev <uzokdeveloper@gmail.com>
2024-07-26 15:50:53 +0500

    PATCH: bux fix

--- app/Telegram/Bot/BotHandler.php
+++ app/Telegram/Bot/BotHandler.php
@@ -12,12 +12,12 @@ class BotHandler extends Bot {
         $user = TgUser::where('user_id', $chatId)->first();
 
         if (!$user){
-            TgUser::create(['user_id' => $chatId]);
+            $user = TgUser::create(['user_id' => $chatId]);
             ReferralCode::create(['tg_user_id' => $user->id, 'code'=>Str::random(10)]);
         }
 
         $this->sendChatAction('typing', $chatId)
-            ->sendMessage('Assalomu alaykum, ');
+            ->sendMessage('Assalomu alaykum, ' . $user->code);
     }
     public function startReferal (int $chatId, string $text):void {
         $referrer_id = explode("/start", $text)[1];
