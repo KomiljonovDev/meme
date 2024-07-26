ee85922aff2054118342bb2655327db99515b58b
Komiljonovdev <uzokdeveloper@gmail.com>
2024-07-26 15:35:07 +0500

    PATCH: future

--- app/Telegram/Bot/BotHandler.php
+++ app/Telegram/Bot/BotHandler.php
@@ -4,15 +4,21 @@ namespace App\Telegram\Bot;
 
 use App\Telegram\Bot\Bot;
 use App\Models\TgUser;
+use App\Models\ReferralCode;
+use Illuminate\Support\Str;
 
 class BotHandler extends Bot {
     public function start(int $chatId):void {
         $user = TgUser::firstOrCreate([
             'user_id' => $chatId,
         ]);
+        $code = ReferralCode::uodateOrCreate([
+            'user_id' => $user->id,
+            'code' => Str::random('10'),
+        ]);
         $this->sendChatAction('typing', $chatId)
             ->sendMessage('Assalomu alaykum');
-        dd($user->id);
+        dd($code);
     }
     public function startReferal (int $chatId, string $text):void {
         $referrer_id = explode("/start", $text)[1];
