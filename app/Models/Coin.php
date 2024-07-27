e948e0ab03fc014b39d39442bbff1e4859291eeb
Komiljonovdev <uzokdeveloper@gmail.com>
2024-07-28 02:55:07 +0500

    PATCH: click api update

--- app/Models/Coin.php
+++ app/Models/Coin.php
@@ -14,7 +14,7 @@ class Coin extends Model
     ];
 
     public static function plus (int $tgUserId) {
-        $referrals = Referral::where('referrer_id', $tgUserId);
+        $referrals = Referral::where('referrer_id', $tgUserId)->toSql();
         dd($referrals);
     }
 }
