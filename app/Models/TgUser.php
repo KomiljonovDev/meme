391d29dcc6791b90952ec1f401cd9c57f3de4e93
Komiljonovdev <uzokdeveloper@gmail.com>
2024-07-28 01:27:27 +0500

    PATCH: saveUser method added

--- app/Models/TgUser.php
+++ app/Models/TgUser.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class TgUser extends Model
 {
@@ -13,4 +14,26 @@ class TgUser extends Model
         'user_id',
         'active'
     ];
+
+    public static function saveUser (int $chatId, $referrer_code = null):array {
+        $user = TgUser::where('user_id', $chatId)->first();
+        if (!$user){
+            $user = TgUser::create(['user_id' => $chatId]);
+            $referral = ReferralCode::create(['tg_user_id' => $user->id, 'code'=>Str::random(10)]);
+            $coin = Coin::create(['tg_user_id' => $user->id, 'coin'=>env('MINIMUM_BONUS_COIND', 10000)]);
+            if ($referrer_code) {
+                $referrer_user = ReferralCode::where(['code' => trim($referrer_code)])->first();
+                Referral::create(['referrer_id' => $referrer_user->tg_user_id, 'referred_user_id' => $user->id]);
+            }
+        }else{
+            $referral = ReferralCode::where('tg_user_id', $user->id)->first();
+            $coin = Coin::where('tg_user_id', $user->id)->first();
+        }
+
+        return [
+            'user' => $user,
+            'referral' => $referral,
+            'coin' => $coin
+        ];
+    }
 }
--- app/Telegram/Bot/BotHandler.php
+++ app/Telegram/Bot/BotHandler.php
@@ -11,19 +11,10 @@ use Illuminate\Support\Str;
 class BotHandler extends Bot {
     public function start(int $chatId):void {
 
-        $user = TgUser::where('user_id', $chatId)->first();
-
-        if (!$user){
-            $user = TgUser::create(['user_id' => $chatId]);
-            $referral = ReferralCode::create(['tg_user_id' => $user->id, 'code'=>Str::random(10)]);
-            $coin = Coin::create(['tg_user_id' => $user->id, 'coin'=>env('MINIMUM_BONUS_COIND', 10000)]);
-        }else{
-            $referral = ReferralCode::where('tg_user_id', $user->id)->first();
-            $coin = Coin::where('tg_user_id', $user->id)->first();
-        }
+        $user_data = TgUser::saveUser($chatId);
 
         $this->sendChatAction('typing', $chatId)
-            ->sendMessage('Assalomu alaykum, ' . $referral->code . "\ncoin: " . $coin->coin);
+            ->sendMessage('Assalomu alaykum, ' . $user_data['referral']->code . "\ncoin: " . $user_data['coin']->coin);
     }
     public function startReferal (int $chatId, string $text):void {
         $referrer_id = explode("/start", $text)[1];
