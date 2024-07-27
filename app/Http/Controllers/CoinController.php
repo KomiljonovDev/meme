8e0c5d204450ee28deba716254f0d2b11d85d84d
Komiljonovdev <uzokdeveloper@gmail.com>
2024-07-28 02:39:15 +0500

    MINOR: click api created

---
+++ app/Http/Controllers/CoinController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CoinController extends Controller
+{
+    public function click(Request $request){
+        $attributes = $request->validate([
+            'tg_user_id'=>'required|integer',
+        ]);
+        dd($attributes);
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
