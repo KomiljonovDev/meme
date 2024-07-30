3d71da54574ae5abbd6757473457853cf5f0ea4b
Komiljonovdev <uzokdeveloper@gmail.com>
2024-07-30 14:50:07 +0500

    MINOR: vue installed

---
+++ meme/src/views/Click.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>All Posts</div>
+    <ul>
+        <li v-for="post in posts">
+            {{post.title}}
+        </li>
+    </ul>
+    <button @click="getPosts">Get Posts</button>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "Click",
+    data(){
+        return {
+            posts:[]
+        }
+    },
+    methods:{
+        getPosts:function(){
+            axios.get("https://jsonplaceholder.typicode.com/posts/")
+                .then(res=>{
+                this.posts = res.data;
+            })
+        }
+    }
+}
+</script>
